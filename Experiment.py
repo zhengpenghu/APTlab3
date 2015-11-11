956652af7cc49476d359fe3d966df791b6aa304b
zhengpenghu <mrhuzp@gmail.com>
2015-11-11 16:49:57 -0600

    add failure

--- Experiment.py
+++ Experiment.py
@@ -1,13 +1,12 @@
 import sys
 
-#add commets to upadte
-
 def largest(list):
-  if len(list) == 0:
+    if len(list) == 0:
     raise ValueError("Cannot call largest on empty list")
-  max = -sys.maxint # "smallest" possible int
-  # max = 0
-  for index in range(len(list)):
-    if (list[index] > max):
-      max = list[index]
-  return max
+        # max = -sys.maxint # "smallest" possible int
+        max = 0
+            for index in range(len(list)):
+if (list[index] > max):
+    max = list[index]
+        return max
+            print "i am done"
